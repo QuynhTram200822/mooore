54fd5c67d6e7cc291d4a2a6f5657c74fe0c48d7a
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2025-07-10 14:53:01 +0700

    upload banner

--- js/home.js
+++ js/home.js
@@ -1,23 +1,23 @@
-jQuery(function($){
-  $('.mv_item').each(function() {
-    if($(this).find('.js_mv_img').length) {
-      var _imgItem = $(this).find('.js_mv_img');
-      var _bgUrl = _imgItem.find('img').attr('src');
-      $(this).css('background-image', 'url(' + _bgUrl + ')');
-      _imgItem.remove();
-    }
-  });
-  $('.hm_mainvisual > div').slick({
-    infinite: true,
-    slidesToShow: 1,
-    arrows: false,
-    // autoplay: true,
-    autoplaySpeed: 5000,
-    speed: 2000,
-    dots: true,
-    pauseOnHover: false
-  });
-  if($('.mv_item').length == 1) {
-    $('.hm_mainvisual .slick-dots').remove();
-  }
-});
+// jQuery(function($){
+//   $('.mv_item').each(function() {
+//     if($(this).find('.js_mv_img').length) {
+//       var _imgItem = $(this).find('.js_mv_img');
+//       var _bgUrl = _imgItem.find('img').attr('src');
+//       $(this).css('background-image', 'url(' + _bgUrl + ')');
+//       _imgItem.remove();
+//     }
+//   });
+//   $('.hm_mainvisual > div').slick({
+//     infinite: true,
+//     slidesToShow: 1,
+//     arrows: false,
+//     // autoplay: true,
+//     autoplaySpeed: 5000,
+//     speed: 2000,
+//     dots: true,
+//     pauseOnHover: false
+//   });
+//   if($('.mv_item').length == 1) {
+//     $('.hm_mainvisual .slick-dots').remove();
+//   }
+// });
